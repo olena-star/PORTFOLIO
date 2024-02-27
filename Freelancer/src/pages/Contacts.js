400e5ab02d94bee7bd4bab5027ff618c1192bf11
olena-star <122024787+olena-star@users.noreply.github.com>
2024-02-27 10:58:32 +0100

    Add files via upload

---
+++ Freelancer/src/pages/Contacts.js
@@ -0,0 +1,25 @@
+const Contacts = () => {
+    return (  <main className="section">
+    <div className="container">
+            <h1 className="title-1">Contacts</h1>
+
+            <ul className="content-list">
+                <li className="content-list__item">
+                    <h2 className="title-2">Location</h2>
+                    <p>Kherson,Ukraine</p>
+                </li>
+                <li className="content-list__item">
+                    <h2 className="title-2">Telegram / WhatsApp</h2>
+                    <p><a href="tel:+79051234567">+4915168430470</a></p>
+                </li>
+                <li className="content-list__item">
+                    <h2 className="title-2">Email</h2>
+                    <p><a href="mailto:webdev@protonmail.com">lena512000@yahoo.com</a></p>
+                </li>
+            </ul>
+
+    </div>
+</main> );
+}
+ 
+export default Contacts;
